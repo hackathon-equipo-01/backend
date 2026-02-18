322ddbf0e631ade462502e960dcde9681e17f92f
Nuclea88 <ingrid.lopov@gmail.com>
2026-02-18 15:08:29 +0100

    feat: connnection to DB

--- src/main/java/com/hackaton/waste/WasteApplication.java
+++ src/main/java/com/hackaton/waste/WasteApplication.java
@@ -3,10 +3,14 @@ package com.hackaton.waste;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class WasteApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(WasteApplication.class, args);
 	}
 
