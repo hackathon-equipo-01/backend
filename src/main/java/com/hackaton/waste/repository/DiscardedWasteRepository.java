357cd8255f6b13ff1df2ca7711d298feb954b151
Nuclea88 <ingrid.lopov@gmail.com>
2026-02-19 13:17:13 +0100

    feat: service and controller

--- src/main/java/com/hackaton/waste/repository/DiscardedWasteRepository.java
+++ src/main/java/com/hackaton/waste/repository/DiscardedWasteRepository.java
@@ -1,9 +1,11 @@
 package com.hackaton.waste.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hackaton.waste.entity.DiscardedWaste;
 
 public interface DiscardedWasteRepository extends JpaRepository<DiscardedWaste, Integer>{
-
+    public List<DiscardedWaste> findByClassroomIdOrderByDateDesc(int classroomId);
 }
--- src/main/java/com/hackaton/waste/service/DiscardedWasteServiceImpl.java
+++ src/main/java/com/hackaton/waste/service/DiscardedWasteServiceImpl.java
@@ -1,13 +1,15 @@
 package com.hackaton.waste.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hackaton.waste.entity.Classroom;
+import com.hackaton.waste.entity.Container;
 import com.hackaton.waste.entity.DiscardedWaste;
-import com.hackaton.waste.repository.ClassroomRepository;
+import com.hackaton.waste.entity.Residue;
 import com.hackaton.waste.repository.DiscardedWasteRepository;
 
 import jakarta.transaction.Transactional;
@@ -16,35 +18,72 @@ import jakarta.transaction.Transactional;
 public class DiscardedWasteServiceImpl implements DiscardedWasteService {
 
     private DiscardedWasteRepository discardedRepository;
+    private ResidueService residueService;
+    private ContainerService containerService;
     private ClassroomService classroomService;
 
-
-    public DiscardedWasteServiceImpl(DiscardedWasteRepository discardedRepository,
-            ClassroomService classroomService ) {
+    public DiscardedWasteServiceImpl(DiscardedWasteRepository discardedRepository, ResidueService residueService,
+            ContainerService containerService, ClassroomService classroomService) {
         this.discardedRepository = discardedRepository;
+        this.residueService = residueService;
+        this.containerService = containerService;
         this.classroomService = classroomService;
     }
 
+    @Override
     @Transactional
-    public DiscardedWaste processWaste(DiscardedWaste record) {
+    public DiscardedWaste processWaste(int idClassroom, int idResidue, int idContainer) {
 
-        boolean isCorrect = record.getResidue().getWasteType()
-                .equals(record.getContainer().getWasteType());
+        Classroom classroom = classroomService.getClassroomById(idClassroom);
+        Residue residue = residueService.getResidueById(idResidue);
+        Container container = containerService.getContainerById(idContainer);
+
+        
+        boolean isCorrect = Objects.equals(residue.getWasteType().getId(), container.getWasteType().getId());
         
         int points = isCorrect ? 10 : -5;
-        if (record.getContainer().getWasteType().getName().equalsIgnoreCase("Resto")) {
-            points = -2; 
+        if (container.getWasteType().getName().equalsIgnoreCase("Resto")) {
+            points = points -2; 
         }
 
-        record.setIsCorrect(isCorrect);
-        record.setPointsEarned(points);
-        record.setDate(LocalDateTime.now());
+        DiscardedWaste dw = new DiscardedWaste();
+        dw.setClassroom(classroom);
+        dw.setResidue(residue);
+        dw.setContainer(container);
+        dw.setIsCorrect(isCorrect);
+        dw.setPointsEarned(points);
+        dw.setDate(LocalDateTime.now());
 
-        Classroom classroom = record.getClassroom();
-        classroom.setPoints(classroom.getPoints() + points);
-        classroomService.addPointsToClassroom(record.getClassroom().getId(), points);
+        classroomService.addPointsToClassroom(idClassroom, points);
 
-        return discardedRepository.save(record);
+        return discardedRepository.save(dw);
+    }
+
+    @Override
+    public List<DiscardedWaste> getHistoryByClassroom(int classroomId) {
+        return discardedRepository.findByClassroomIdOrderByDateDesc(classroomId);
+    }
+    @Override
+    public List<DiscardedWaste> getAllDiscardedWastes(){
+        return discardedRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void deleteDiscardedWaste(int id) {
+        DiscardedWaste dw = discardedRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Registro no encontrado"));
+
+        classroomService.addPointsToClassroom(dw.getClassroom().getId(), -dw.getPointsEarned());
+
+        discardedRepository.delete(dw);
+    }
+
+    @Override
+    public DiscardedWaste getDiscardedWasteById(int id) {
+        return discardedRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("objeto no encontrado con ID: " + id));
+    
     }
 }
 
